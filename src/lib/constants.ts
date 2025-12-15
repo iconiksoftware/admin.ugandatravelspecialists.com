9b3b25d71a065de0478b6b7b2978f14430f27802
Shindra David <shindradavid.work@gmail.com>
2025-12-15 19:55:21 +0300

    Change api domain

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
 // export const API_BASE_URL = 'https://api.ugandatravelspecialist.travelmaster.cloud';
-export const API_BASE_URL = 'http://192.168.1.24:8011';
+export const API_BASE_URL = 'https://api.ugandatravelspecialists.com';
+//export const API_BASE_URL = 'http://192.168.1.24:8011';
 
 export const SESSION_ID_KEY = 'sessionId';
